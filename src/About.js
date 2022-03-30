3274a2edeba4808d4b55eb9b813a524003d179e4
ali-masaaf <ali.masaaf.profile@gmail.com>
2022-03-30 14:01:58 +0000

    feat: styling about page

--- src/About.js
+++ src/About.js
@@ -6,23 +6,23 @@ const About = () => {
             <div className="about_content">
                 <h1  className='content_titel'>I'M</h1>
                 <div className='content_subject'>
-
+                    <h1  className='subject_text'>
+                        <span className='text_name'>Ali MASAAF</span> UI/UX<br/>
+                        Designer and<br/>
+                        Full-stack web <br/>
+                        Developer<br/>
+                        With a masters degree in<br/>
+                        Information system<br/>
+                        Management and a 42 <br/>
+                        Schools network student <br/>
+                        At 1337 campuses I enjoy<br/>
+                        Watching Japeneis anime<br/>
+                        And I like innovative <br/>
+                        Technologies and designing.<br/>
+                    </h1>
+                    <i className="subject_icon-smile"/>
                 </div>
-                <h1  className='subject_text'>
-                    Ali MASAAF UI/UX
-                    Designer and
-                    Full-stack web 
-                    Developer
-                    With a masters degree in
-                    Information system
-                    Management and a 42 
-                    Schools network student 
-                    At 1337 campuses I enjoy
-                    Watching Japeneis anime
-                    And I like innovative 
-                    Technologies and designing.
-                </h1>
-                <i className="subject_icon-smile"/>
+
             </div>
             <SideBar class="about"/>
         </div> 
