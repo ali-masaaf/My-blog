434e3a4baf3705fbdeaed0566e0bc489505528ae
ali-masaaf <ali.masaaf.profile@gmail.com>
2022-04-01 01:38:17 +0000

    fix: unify My Blog text font size

--- src/Contact.js
+++ src/Contact.js
@@ -1,23 +1,27 @@
 import SideBar from './components/SideBar.js'
+import {Link} from 'react-router-dom'
 
 const Contact = () => {
     return ( 
         <div className="app_contact">
             <div className="contact_content">
-                <h1  className='content_titel'>
-                    My<br/>
-                    Blog
-                </h1>
+                <Link to="/">
+                    <h1  className='content_titel'>
+                        My<br/>
+                        Blog
+                    </h1>
+                </Link>
+                
                 <div className='content_subject'>
                     <h1  className='subject_text'>
                         Find Me<br/>
                         On
                     </h1>
                     <div className='subject_icons'>
-                        <i className="icons_linkedin"/>
-                        <i className="icons_github"/>
-                        <i className="icons_dribbble"/>
-                        <i className="icons_behance"/>
+                        <a href="https://www.linkedin.com/in/ali-masaaf/"><i className="icons_linkedin"/></a>
+                        <a href="https://github.com/ali-masaaf"><i className="icons_github"/></a>
+                        <a href=""><i className="icons_dribbble"/></a>
+                        <a to=""><i className="icons_behance"/></a>
                     </div>
                 </div>
             </div>
