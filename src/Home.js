4c877f701d5df3a003884793e8601e9fe64555ce
ali-masaaf <ali.masaaf.profile@gmail.com>
2022-07-06 12:47:50 +0100

    feat: apply the mask only on the first blog card

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,20 @@ import useFetsh from "./hooks/useFetch"
 
 const Home = () => {
     const {data:blogs} = useFetsh("http://localhost:5000/blogs");
+    let x = 0;
+
+    const handleScroll = (childs)=>{
+        for (let i = 0; i < childs.length; i++) {
+            x = childs[i].getBoundingClientRect().x;
+            if (x < 225){
+                console.log("Clear:" + childs[i].className);
+                childs[i].firstChild.style.display = "none";
+            }else{
+                childs[i].firstChild.style.display = "inline-block";
+            }
+        }
+    }
+
     return ( 
         <div className="app_home">
             <div className="home_content">
@@ -18,7 +32,7 @@ const Home = () => {
                     <i className='button_menu-icon'/>
                 </Link>
 
-                <div className='content_subject'>
+                <div className='content_subject' onScroll={(e)=>{handleScroll(e.target.firstChild.children)}}>
                     {blogs && <BlogsList className="subject_blogs-list" blogs={blogs}/>}
                 </div>
             </div>
